6a96572dd70fc94b25b5dd6a395bff1abf6582d7
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-19 22:25:02 +0000

    datatypes

---
+++ 02-variable.js
@@ -0,0 +1,11 @@
+const  accountId = '1234567'
+let emailId = 'ali.google@.com'
+var password = '222222'
+accountCity = 'karachi'
+
+emailId = 'mah.google@accountId.com'
+password = '4444'
+accountCity = 'Multan'
+
+
+console.table([accountId,emailId,password,accountCity])
---
+++ 03-datatypes.js
@@ -0,0 +1,32 @@
+ 'use strict'; //treat all js code as newer version
+// alert(3+3) // we are using node.js not a browser
+
+// console.log(3+3)
+
+let name = "Ali"
+let age = 18
+let loggedIn = true
+
+
+
+//(Premetive datatypes)
+//number=> 2 to power 53
+//bigint
+//boolean => "true" / "False"
+//string => ""
+//null => standa lone values
+//undefined => 
+//symbol => unique
+
+
+console.log(typeof "Ali" )
+
+
+
+
+
+
+
+
+
+
