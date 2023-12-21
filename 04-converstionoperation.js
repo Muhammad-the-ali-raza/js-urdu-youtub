ac458c1f65ca65236f30772d92406adce6384ed8
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-21 21:32:50 +0000

    converstion of datatypes

---
+++ 04-converstionoperation.js
@@ -0,0 +1,15 @@
+let isLoggedIn = "Hitesh"
+
+// let boleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn)
+
+
+// 1 => true, 0 => false
+// "" => false    
+// Hitesh => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
