beb2444b92cabd77758829f4002ea73de91033ed
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-21 22:30:54 +0000

    javascriptoperations

---
+++ 05-javascriptoperations.js
@@ -0,0 +1,13 @@
+// ******************** OPERATIONS ******************
+
+let str1 = "hello"
+let str2 =  " hey"
+let str3 = str1 + str2
+// let str3 = "hello"+ " hey"
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
