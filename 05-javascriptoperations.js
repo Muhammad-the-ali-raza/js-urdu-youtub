7c53e7b5a85dffcdbed7f5b7f4352f485a84c74d
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-23 14:18:59 +0000

    joperations

--- 05-javascriptoperations.js
+++ 05-javascriptoperations.js
@@ -1,13 +1,18 @@
-// ******************** OPERATIONS ******************
+// // ******************** OPERATIONS ******************
 
-let str1 = "hello"
-let str2 =  " hey"
-let str3 = str1 + str2
-// let str3 = "hello"+ " hey"
-console.log(str3)
+// let str1 = "hello"
+// let str2 =  " hey"
+// let str3 = str1 + str2
+// // let str3 = "hello"+ " hey"
+// console.log(str3)
 
 
-console.log("1" + 2)
-console.log(1 + "2")
-console.log("1" + 2 + 2)
-console.log(1 + 2 + "2")
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+let value = 33;
+let negvalue = -value
+console.log(negvalue)
---
+++ 06-operations.js
@@ -0,0 +1,32 @@
+// // ********************** OPERATIONS *************************
+
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1,num2,num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num1,num2,num3)
+
+// let gameCounter = 100
+
+// gameCounter++;
+// console.log(gameCounter)
+
+
+// let x = 3;
+// const y = x++;
+
+// console.log('x:${x} , y:${y}');
+
+let x = 3
+const y = x++
+
+console.log (3)
+console.log(x++)
+
+// let a = 3;
+// const b = ++b;
+
+// console.log('a:${a} , b:${b}');
