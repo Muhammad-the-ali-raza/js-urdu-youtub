345e016187edda1c041c716868b9c28aed45f3f6
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-23 15:16:24 +0000

    datatypes-summary

---
+++ 08-datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitve datatypes
+// String,numbers,boolean,null,
+
+
+// const score = 100
+// const scorevalue = 10.32
+
+
+const isLoggedIn = false
+const outSideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id == anotherId);
+
+
+// non primitive datatypes
+
+Arrays, Objects, Function
