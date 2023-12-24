e034019502347805224c655cad62dec6e5355b93
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-24 15:02:22 +0000

    datatypes-summaary

--- 08-datatypes-summary.js
+++ 08-datatypes-summary.js
@@ -1,22 +1,35 @@
-// primitve datatypes
-// String,numbers,boolean,null,
+// // primitve datatypes
+// // String,numbers,boolean,null,
 
 
-// const score = 100
-// const scorevalue = 10.32
+// // const score = 100
+// // const scorevalue = 10.32
 
 
-const isLoggedIn = false
-const outSideTemp = null
-let useremail;
+// const isLoggedIn = false
+// const outSideTemp = null
+// let useremail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-
-
-console.log(id == anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
 
+// console.log(id == anotherId);
+
+const bigNumber = 2492488421981n
 // non primitive datatypes
 
-Arrays, Objects, Function
+// Arrays, Objects, Function
+
+
+const  hero = ['Superman', 'Aquaman', 'Ironman']
+let myobj = {
+    name : 'Thor',
+    age :33,
+}
+
+const Function = function(){
+    console.log('Hello World')
+}
+
+console.log(typeof Function)
