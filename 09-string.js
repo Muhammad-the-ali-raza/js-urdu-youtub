bd575d6899028caf47f2c3165fcc1c277c1b8f3c
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-28 19:28:03 +0000

    strings

--- 09-string.js
+++ 09-string.js
@@ -1,24 +1,39 @@
-const name = 'Hitesh'
-const repocount = 50
+// const name = 'Hitesh'
+// const repocount = 50
 
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
 
-console.log(`${name} + ${repocount} + "Value" `)
+// console.log(`${name} + ${repocount} + "Value" `)
 
-console.log(name + repocount + "Value" );
+// console.log(name + repocount + "Value" );
 
-const nameGame = new String (`ALIraza`)
-console.log(nameGame)
-// console.log(nameGame[0][1])
-console.log(nameGame.__proto__)
+// const nameGame = new String (`ALIraza`)
+// console.log(nameGame)
+// // console.log(nameGame[0][1])
+// console.log(nameGame.__proto__)
 
-console.log(nameGame.toUpperCase())
-// console.log(nameGame.toLocaleLowerCase())
-console.log(nameGame.toLowerCase())
-console.log(nameGame.charAt(5))
+// console.log(nameGame.toUpperCase())
+// // console.log(nameGame.toLocaleLowerCase())
+// console.log(nameGame.toLowerCase())
+// console.log(nameGame.charAt(5))
 
 // const newString = nameGame.substring(0,4)
 // console.log(newString)
 
-const anotherString1 = nameGame.slice(0,4)
-console.log(newString1)
+// const anotherString1 = nameGame.slice(0,4)
+// console.log(newString1)
+
+const stringnumberone = "    Ali    "
+console.log(stringnumberone)
+console.log(stringnumberone.trim())
+
+
+const url = "https://Ali.com/Ali%20raza"
+console.log(url)
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('uzair'))
+
+
+
+
