a852e95007768ee80cbe66cafa34761900de9c6d
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2023-12-28 19:27:30 +0000

    math and num

---
+++ 10-num-and-math.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 5794.4984
+// console.log(otherNumber.toPrecision(4));
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-US'))
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))    //its a absolute value
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(5.3))
+// console.log(Math.floor(8.3))
+// console.log(Math.min (2,3,4,5,6,7,8,1))
+// console.log(Math.max (2,3,4,5,6,7,8,1))
+
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 20
+const max = 10
+
+console.log(Math.floor (Math.random() * (max-min +1)) + min )
+
+
