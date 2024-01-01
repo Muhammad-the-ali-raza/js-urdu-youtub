a6c50b38eded9e43ff6b5ba41bc81f2b482ac5ed
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2024-01-01 18:46:02 +0000

    arrays

---
+++ 12-arrays.js
@@ -0,0 +1,37 @@
+// array
+
+const myArr = [5,78,34,3,7,1]
+
+const myHeros = ["IronMan", "Hulk", "Thor"]
+
+// const newArr2 = new Array (1,2,3,4)
+
+// console.log(myArr[4])
+
+// console.log (myHeros[1])
+
+// console.log (newArr2 [1])
+
+// Arrays Method
+
+// myArr.push()
+// myArr.pop()  //this method functionality is last value output  is removed \
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(3))
+// console.log(myArr.indexOf(1))
+
+// console.log(myArr)
+
+const newArr = myArr.join()
+// console.log(newArr)
+
+// console.log(typeof newArr)
+
+console.log("A" , myArr)
+
+
+const myn1 = myArr.slice (1,3)
+console.log(myn1)
+console.log("B" , myArr)
