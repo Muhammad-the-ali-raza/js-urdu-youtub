f41fe230be58fc2312e793d445648cb0efd78239
Muhammad-the-ali-raza <154095484+Muhammad-the-ali-raza@users.noreply.github.com>
2024-01-11 20:32:35 +0000

    JSON

--- 13-objects.js
+++ 13-objects.js
@@ -9,26 +9,25 @@ const {courseInstructor : Ali} = course
 // console.log(Ali)
 
 const regularuser = {
-    email : 'ali@google.com',
-    fullName : {
-        userFullName : {
-            firstName : 'ali',
-            lastName : 'raza',
+    email : "ali@google.com",
+    fullname : {
+        userfullname : {
+            firstname : "ali",
+            lastname : "raza",
         }
     }
 }
 
-console.log(regularuser.email.userFullName)
+console.log(regularuser.fullname)
 
+const obj1 = {1 : 'a', 2 : 'b'}
 
+const obj2 = {3 : 'c', 4 : 'd'}
 
+// const obj3 = {obj1, obj2}
 
+// const obj3 = Object.assign( {}, obj1, obj2)
 
+const obj3 = {...obj1, ...obj2}
 
-
-
-
-
-
-
-
+console.log(obj3)
